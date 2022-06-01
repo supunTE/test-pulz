fab376b56b8feb5a27b630a20cedb233d4ef562f
STEdirisuriya <supunedirisuriya2002@gmail.com>
2022-06-01 20:29:40 +0530

    initialized

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+// var parallaxInstance = new Parallax(scene, {
+//   relativeInput: true
+// });
+
+// parallaxInstance.friction(0.2, 0.2);
